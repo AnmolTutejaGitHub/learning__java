b211412e2f1946fe280da619c85e9aa04bc2b385
Anmol Tuteja <tutejaanmol54@gmail.com>
2023-12-25 15:33:16 +0530

    Update TernaryOperator.java

--- TernaryOperator.java
+++ TernaryOperator.java
@@ -7,6 +7,9 @@ public class TernaryOperator {
         // (makeOfCar == "Volkswagen") is condition to be evaluated ; if this condition
         // is true return operand2 else operand3
 
+        //can also do : 
+        //boolean isDomestic = (makeOfCar != "Volkswagen");
+
         /////////////////////////////////////////////////
         if (isDomestic) {
             System.out.println("This car is domestic to our country");
