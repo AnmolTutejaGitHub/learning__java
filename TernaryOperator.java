853a1726df3cdbd693821216fce73a872c4a96ac
Anmol Tuteja <tutejaanmol54@gmail.com>
2023-12-25 15:27:22 +0530

    Add files via upload

---
+++ TernaryOperator.java
@@ -0,0 +1,19 @@
+public class TernaryOperator {
+    public static void main(String[] args) {
+
+        // operan1 ? operand2:operand3;
+        String makeOfCar = "Volkswagen";
+        boolean isDomestic = (makeOfCar == "Volkswagen") ? false : true; // false
+        // (makeOfCar == "Volkswagen") is condition to be evaluated ; if this condition
+        // is true return operand2 else operand3
+
+        /////////////////////////////////////////////////
+        if (isDomestic) {
+            System.out.println("This car is domestic to our country");
+        }
+
+        String s = (isDomestic) ? "This car is domestic" : "This car is not domestic";
+
+        System.out.println(s); // This car is not domestic
+    }
+}
